13b19a2a6cc456eeabeb040de25521314d7b694b
iroha <iroha@RUI.>
2026-03-11 15:06:17 +0800

    lab1_q1circle

---
+++ cits3003_labs/cmake-build-debug/CMakeFiles/lab1_q1circle.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_q1circle.dir/helpers/ShaderHelper.cpp.o"
+  "CMakeFiles/lab1_q1circle.dir/helpers/ShaderHelper.cpp.o.d"
+  "CMakeFiles/lab1_q1circle.dir/lab1/q1circle.cpp.o"
+  "CMakeFiles/lab1_q1circle.dir/lab1/q1circle.cpp.o.d"
+  "lab1_q1circle"
+  "lab1_q1circle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_q1circle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
