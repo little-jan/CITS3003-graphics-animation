8cab9ce858797b5848357650392e8c7f242ca95d
iroha <iroha@RUI.>
2026-03-11 14:19:50 +0800

    initial building via cmake

---
+++ cits3003_labs/cmake-build-debug/CMakeFiles/lab1_example1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_example1.dir/helpers/ShaderHelper.cpp.o"
+  "CMakeFiles/lab1_example1.dir/helpers/ShaderHelper.cpp.o.d"
+  "CMakeFiles/lab1_example1.dir/lab1/example1.cpp.o"
+  "CMakeFiles/lab1_example1.dir/lab1/example1.cpp.o.d"
+  "lab1_example1"
+  "lab1_example1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_example1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cits3003_labs/cmake-build-debug/CMakeFiles/lab1_example2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_example2.dir/helpers/ShaderHelper.cpp.o"
+  "CMakeFiles/lab1_example2.dir/helpers/ShaderHelper.cpp.o.d"
+  "CMakeFiles/lab1_example2.dir/lab1/example2.cpp.o"
+  "CMakeFiles/lab1_example2.dir/lab1/example2.cpp.o.d"
+  "lab1_example2"
+  "lab1_example2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_example2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
